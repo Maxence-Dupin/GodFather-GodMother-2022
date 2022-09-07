dffbb351ee7656c3efe45a98b5fcf834ee3fbca1
Maxence Dupin <maxencedupin001@gmail.com>
2022-09-07 14:04:23 +0200

    Sprint

--- GodFatherGodMother2022/Assets/Scripts/PlayerControls.cs
+++ GodFatherGodMother2022/Assets/Scripts/PlayerControls.cs
@@ -6,17 +6,34 @@ public class PlayerControls : MonoBehaviour
 {
 
     [SerializeField] private int speed;
+    [SerializeField] private int sprintSpeed;
     [SerializeField] private int rotationSpeed;
 
     public List<Item> inventory;
     public Item currentItem;
 
+    [SerializeField] float stamina = 100;
+    [SerializeField] float staminaLossPerSecond;
+    [SerializeField] float staminaGainPerSecond;
+
     // Update is called once per frame
     void Update()
     {
         //déplacement du perso
-        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(0, 0, translation);
+        if(Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+            Debug.Log("shift");
+            float translation = Input.GetAxis("Vertical") * sprintSpeed * Time.deltaTime;
+            transform.Translate(0, 0, translation);
+            stamina -= staminaLossPerSecond * Time.deltaTime;
+            stamina = Mathf.Clamp(stamina, 0, 100);
+        }
+        else
+        {
+            float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            transform.Translate(0, 0, translation);
+        }
+
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
         transform.Rotate(0, rotation, 0);
 
