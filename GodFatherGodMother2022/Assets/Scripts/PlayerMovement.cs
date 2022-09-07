21c3e0dc18bba925d6ffa966fd616076bf5f08e2
RakanAD <zhangroland385@gmail.com>
2022-09-07 10:27:39 +0200

    Player Movement

---
+++ GodFatherGodMother2022/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 playerVelocity;
+    public float speed = 3f;
+    public float speedRotation = 3f;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DoMovement(Vector2 input)
+    {
+        /*Vector3 rotatePlayer = Vector3.zero;*/
+        Vector3 moveDirection = Vector3.zero;
+        /*rotatePlayer.x = input.x;*/
+        moveDirection.z = input.y;
+        characterController.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        transform.Rotate(0f, speedRotation * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+    }
+}
