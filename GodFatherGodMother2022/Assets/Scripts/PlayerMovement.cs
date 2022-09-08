4e889da8f1731c70ac07825c401125532bb6cf27
Maxence Dupin <maxencedupin001@gmail.com>
2022-09-08 11:21:04 +0200

    Tomb integration + sprint fix

--- GodFatherGodMother2022/Assets/Scripts/PlayerMovement.cs
+++ GodFatherGodMother2022/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
     private Vector3 playerVelocity;
-    public float speed = 3f;
+    public float speed = 2f;
+    public float sprintSpeed = 5f;
     public float speedRotation = 3f;
+    public PlayerControls playerControls;
 
     public Animator headBob;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        playerControls = GetComponent<PlayerControls>();
     }
 
     void Update()
@@ -25,7 +28,14 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 moveDirection = Vector3.zero;
         moveDirection.z = input.y;
-        characterController.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        if(playerControls.canSprint)
+        {
+            characterController.Move(transform.TransformDirection(moveDirection) * sprintSpeed * Time.deltaTime);
+        }
+        else
+        {
+            characterController.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        }
         transform.Rotate(0f, speedRotation * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
 
         headBob.SetTrigger("Walk");
--- GodFatherGodMother2022/Assets/Scripts/Tomb.cs
+++ GodFatherGodMother2022/Assets/Scripts/Tomb.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
 using UnityEngine.UI;
 
-public class Chest : MonoBehaviour
+public class Tomb : MonoBehaviour
 {
 
     private PlayerControls player;
     [SerializeField] private bool isInTrigger = false;
+    [SerializeField] private bool isOpen = false;
     [SerializeField] private Text interactText;
-    [SerializeField] private GameObject treasure;
+    [SerializeField] private GameObject shovel;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip audioClip;
+    [SerializeField] private Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +26,26 @@ public class Chest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isInTrigger && player.currentItem.itemName == "key")
+        if (isInTrigger && !isOpen)
         {
             interactText.gameObject.SetActive(true);
-            interactText.text = "Press E to open the chest";
+            interactText.text = "Press E to open the tomb";
             if (Input.GetKeyDown(KeyCode.E))
             {
+                isOpen = true;
+                animator.SetBool("IsOpen", true);
+                StartCoroutine(StopAnimation());
                 interactText.gameObject.SetActive(false);
-                treasure.SetActive(true);
+                shovel.SetActive(true);
                 audioSource.PlayOneShot(audioClip);
-                Destroy(this.gameObject);
             }
         }
+    }
+
+    public IEnumerator StopAnimation()
+    {
+        yield return new WaitForSeconds(5f);
+        animator.speed = 0;
     }
 
     private void OnTriggerEnter(Collider other)
